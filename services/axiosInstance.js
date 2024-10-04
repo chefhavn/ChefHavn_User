c9e387f59e9787e4691341fc6c77ff087bcac7f5
chefhavn <devservices.chefhavn@gmail.com>
2024-10-04 12:29:23 +0530

    Update axiosInstance.js

--- services/axiosInstance.js
+++ services/axiosInstance.js
@@ -2,14 +2,14 @@
 import axios from 'axios';
 // import { DEVELOPMENT_URL, PRODUCTION_URL } from '@env';
 
-const DEVELOPMENT_URL = 'http://192.168.0.45:3000';
-const PRODUCTION_URL = 'https://www.chefhavn.com';
+// const DEVELOPMENT_URL = 'http://192.168.0.45:3000';
+// const PRODUCTION_URL = 'https://www.chefhavn.com';
 
 // Define your base URL based on environment
-const isDevelopment = __DEV__;
+// const isDevelopment = __DEV__;
 // const isDevelopment = '';
-const BASE_URL = isDevelopment ? DEVELOPMENT_URL : PRODUCTION_URL;
-// const BASE_URL = 'https://www.chefhavn.com';
+// const BASE_URL = isDevelopment ? DEVELOPMENT_URL : PRODUCTION_URL;
+const BASE_URL = 'https://www.chefhavn.com';
 
 // Create an Axios instance
 const axiosInstance = axios.create({
